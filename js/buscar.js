dbd05be9e722cc25c9ba32cea3650a5304e115c4
sarinhaw <sarah.oliveira@sptech.school>
2024-04-18 21:33:07 -0300

    atualização buscar

--- js/buscar.js
+++ js/buscar.js
@@ -1,6 +1,5 @@
 
 async function buscar(){
-    console.log("Passei por aqui")
 
     const resposta = await fetch("https://660f44b0356b87a55c510d90.mockapi.io/agendas")
     const respostaDadosAgenda = await resposta.json();
@@ -11,11 +10,40 @@ async function buscar(){
 
     cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
 
-        return " TESTE "
+        return `
+        <div class="cardItem">
+        <div class="dataGame"> 
+            <img src="../imagens/calendar-solid.svg" alt=""> ${itemAgenda.dataJogo}
+        </div>
+        <img class="img-background" src="${itemAgenda.urlImagem}" alt="">
+        <div class="descricao">
+            <p> <strong>${itemAgenda.nome}</strong> </p>
+            <p>${itemAgenda.descricao} </p>
+            <p><strong>Gamers: </strong></p>
 
-    });
+            <div class="gamers">
+
+            ${
+                itemAgenda.gamers.map((gamerItem) => {
+
+                    return `
+                    <div class="gamerItem">@${gamerItem}</div>
+                    
+                    `
+
+                }).join('')
+            }
+            
+            </div>
+          
+        </div>  
+        <div class="assistir">
+                <a class="assistirItem"> <img src="../imagens/youtube.svg" alt=""> Assistir </a>
+        </div>
+    </div>
+
+        `
+    }).join('');
 }
 
-console.log("antes de buscar")
 buscar();
-console.log("depois de buscar")
