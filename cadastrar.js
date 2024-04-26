5c30839be5314801cb3b0d75b60cf4bfebf68e8c
sarinhaw <sarah.oliveira@sptech.school>
2024-04-25 21:46:25 -0300

    última aula bootcamp

---
+++ cadastrar.js
@@ -0,0 +1,32 @@
+async function cadastrarAgenda(event){
+    event.preventDefault();
+        const nomeDigitado = document.getElementById("nome_jogo").value;
+        const dataDigitada = document.getElementById("data_jogo").value;
+        const descricaoDigitada = document.getElementById("descricao").value;
+        const gamersDigitados = document.getElementById("gamers").value;
+        const urlImgDigitada = document.getElementById("url_imagem").value;
+        const urlLiveDigitada = document.getElementById("url_live").value;
+    
+        const dadosAgenda = {       
+            "nome": nomeDigitado, 
+            "dataJogo": dataDigitada,
+            "descricao": descricaoDigitada,
+            "gamers": gamersDigitados.split(','),
+            "urlAssistir": urlLiveDigitada,
+            "urlImagem": urlImgDigitada,
+        }
+    
+        const respostaCadastro = await fetch("https://660f44b0356b87a55c510d90.mockapi.io/agendas", {
+            method: "POST",
+            body: JSON.stringify(dadosAgenda),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        });
+    
+        if(respostaCadastro.status == 201){
+            window.location.href = "agenda.html";
+        }else{
+            alert("Ocorreu um erro ao cadastrar")
+        }
+    }
+
+
